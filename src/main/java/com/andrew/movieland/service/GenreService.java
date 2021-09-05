ac30969668e42089d1a59ccc744c498cfceaa7a2
Anur13 <akrupskyi13@gmail.com>
2021-09-06 00:24:23 +0300

    WIP

--- src/main/java/com/andrew/movieland/service/GenreService.java
+++ src/main/java/com/andrew/movieland/service/GenreService.java
@@ -2,6 +2,7 @@ package com.andrew.movieland.service;
 
 import com.andrew.movieland.dao.GenreDao;
 import com.andrew.movieland.entity.Genre;
+import com.andrew.movieland.service.cache.GenreCache;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,11 +13,14 @@ public class GenreService {
     @Autowired
     private GenreDao genreDao;
 
+    @Autowired
+    private GenreCache genreCache;
+
     public List<Genre> findAll() {
         return genreDao.findAll();
     }
 
     public Genre findById(int id) {
-        return genreDao.findById(id);
+        return genreCache.getGenresList().get(id);
     }
 }
